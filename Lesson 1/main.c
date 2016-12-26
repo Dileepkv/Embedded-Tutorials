527b4c599a7c23ee36975660fcc07aefad88c156
reversenorm <jphillips01@gmail.com>
2016-12-25 18:56:22 -0700

    Iterate counter for step through
    ++ is a thing.

--- Lesson 1/main.c
+++ Lesson 1/main.c
@@ -1,4 +1,24 @@
 int main(){
     int counter = 0 ;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
     return 0;
 }
