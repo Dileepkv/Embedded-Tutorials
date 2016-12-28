69979e99b764b9e897c6a6eaf68a4274a5c95379
reversenorm <jphillips01@gmail.com>
2016-12-27 20:57:20 -0700

    Include a header and use

--- Lesson 5/main.c
+++ Lesson 5/main.c
@@ -1,38 +1,32 @@
-#define RCGCGPIO (*((unsigned int *)0x400FE608U))
-
-#define GPIOF_BASE 0x40025000U
-#define GPIOF_DIRECTION (*((unsigned int *) (GPIOF_BASE + 0x400U)))
-#define GPIOF_DEN (*((unsigned int *) (GPIOF_BASE + 0x51CU)))
-#define GPIOF_COLORVALUE (*((unsigned int *) (GPIOF_BASE + 0x3FCU)))
-
-
+#include "lm4f120h5qr.h"
 
 int main(){
-    RCGCGPIO = 0x20U ; /*turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory*/ 
-    GPIOF_DIRECTION = 0x0EU ; /*turn on bits 1-3 of 0x40025400 for pin direction*/
-    GPIOF_DEN = 0x0EU ; /*turn on bits 1-3 of 0x4002551c to set pins as data output */
-    
+    SYSCTL_RCGCGPIO_R = 0x20U ; /*turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory*/ 
+    GPIO_PORTF_DIR_R = 0x0EU ; /*turn on bits 1-3 of 0x40025400 for pin direction*/
+    GPIO_PORTF_DEN_R = 0x0EU ; /*turn on bits 1-3 of 0x4002551c to set pins as data output */
+    GPIO_PORTF_DATA_R = 0x02U ; /*set the red bit for the LED on */
+       
     while(1){
-        GPIOF_COLORVALUE = 0x02U ; /*set the red bit for the LED on */
-        int counter = 0;
-        while(counter < 1000000){
+        GPIO_PORTF_DATA_R = 0x02U ; /*set the red bit for the LED on */
+        int volatile counter = 0;
+        while(counter < 1000){
           ++counter;
         }
         
-        GPIOF_COLORVALUE = 0x04U ; /*set the blue bit for the LED on */
+        GPIO_PORTF_DATA_R = 0x04U ; /*set the blue bit for the LED on */
         
         counter = 0;
         while(counter < 1000000){
           ++counter;
         }
         
-        GPIOF_COLORVALUE = 0x08U ; /*set the green bit for the LED on */
+        GPIO_PORTF_DATA_R = 0x08U ; /*set the green bit for the LED on */
         counter = 0;
         while(counter < 1000000){
           ++counter;
         }
         
-        GPIOF_COLORVALUE = 0x00U ; /*set the green bit for the LED off */
+        GPIO_PORTF_DATA_R = 0x00U ; /*set the green bit for the LED off */
         counter = 0;
         while(counter < 1000000){
           ++counter;
