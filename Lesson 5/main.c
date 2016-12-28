f54c59de131229b3e95c913b9853fc45c1f60335
reversenorm <jphillips01@gmail.com>
2016-12-27 20:34:41 -0700

    Learning #define
    Macros!!!
    
    Not the 90's anime Macross.

--- Lesson 5/main.c
+++ Lesson 5/main.c
@@ -1,29 +1,38 @@
+#define RCGCGPIO (*((unsigned int *)0x400FE608U))
+
+#define GPIOF_BASE 0x40025000U
+#define GPIOF_DIRECTION (*((unsigned int *) (GPIOF_BASE + 0x400U)))
+#define GPIOF_DEN (*((unsigned int *) (GPIOF_BASE + 0x51CU)))
+#define GPIOF_COLORVALUE (*((unsigned int *) (GPIOF_BASE + 0x3FCU)))
+
+
+
 int main(){
-    *((unsigned int *)0x400FE608U)= 0x20U ; /*turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory*/ 
-    *((unsigned int *)0x40025400U)= 0x0EU ; /*turn on bits 1-3 of 0x40025400 for pin direction*/
-    *((unsigned int *)0x4002551CU)= 0x0EU ; /*turn on bits 1-3 of 0x4002551c to set pins as data output */
+    RCGCGPIO = 0x20U ; /*turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory*/ 
+    GPIOF_DIRECTION = 0x0EU ; /*turn on bits 1-3 of 0x40025400 for pin direction*/
+    GPIOF_DEN = 0x0EU ; /*turn on bits 1-3 of 0x4002551c to set pins as data output */
     
     while(1){
-        *((unsigned int *)0x400253FCU)= 0x02U ; /*set the red bit for the LED on */
+        GPIOF_COLORVALUE = 0x02U ; /*set the red bit for the LED on */
         int counter = 0;
         while(counter < 1000000){
           ++counter;
         }
         
-        *((unsigned int *)0x400253FCU)= 0x04U ; /*set the blue bit for the LED on */
+        GPIOF_COLORVALUE = 0x04U ; /*set the blue bit for the LED on */
         
         counter = 0;
         while(counter < 1000000){
           ++counter;
         }
         
-        *((unsigned int *)0x400253FCU)= 0x08U ; /*set the green bit for the LED on */
+        GPIOF_COLORVALUE = 0x08U ; /*set the green bit for the LED on */
         counter = 0;
         while(counter < 1000000){
           ++counter;
         }
         
-        *((unsigned int *)0x400253FCU)= 0x00U ; /*set the green bit for the LED off */
+        GPIOF_COLORVALUE = 0x00U ; /*set the green bit for the LED off */
         counter = 0;
         while(counter < 1000000){
           ++counter;
