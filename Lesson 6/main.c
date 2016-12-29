25bce090c02c6f39c341185602d24de1e158f0bc
reversenorm <jphillips01@gmail.com>
2016-12-28 21:12:40 -0700

    Keep blue on flash red
    blue - purple - blue - purple etc.

--- Lesson 6/main.c
+++ Lesson 6/main.c
@@ -1,40 +1,24 @@
 #include "lm4f120h5qr.h"
 
+#define LED_RED (1u << 1) //this defines the bit value associated with red for the LED data register 0x02u
+#define LED_BLUE (1u << 2) //0x04u
+#define LED_GREEN (1u << 3) //0x08u
+
 int main(){
-    SYSCTL_RCGCGPIO_R = 0x20U ; //turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory 
-    GPIO_PORTF_DIR_R = 0x0EU ; //turn on bits 1-3 of 0x40025400 for pin direction
-    GPIO_PORTF_DEN_R = 0x0EU ; //turn on bits 1-3 of 0x4002551c to set pins as data output 
-    GPIO_PORTF_DATA_R = 0x02U ; //set the red bit for the LED on
-    
-    unsigned int a=0x5a5a5a5a;
-    unsigned int b=0xDEADBEEF;
-    unsigned int c;
-    
-    c = a | b; //or
-    c = a ^ b; //exclusive or
-    c = a & b; //and
-    c = ~b;   //NOT or compliment
-    c = b >> 1; //right-shift 
-    c= b << 3; //left-shift
-    
-    int x =1024;
-    int y =-1024;
-    int z;
-    
-    z= x >> 3;
-    z= y >> 3;
-    
-    
-      
-    GPIO_PORTF_DATA_R = 0x04U ; //turn blue on before loop 
+    SYSCTL_RCGCGPIO_R |= (1u << 5) ; //turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory 
+    GPIO_PORTF_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x40025400 for pin direction
+    GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x4002551c to set pins as data output 
+
+    GPIO_PORTF_DATA_R |= LED_BLUE ;  
     while(1){
-        GPIO_PORTF_DATA_R = 0x02U ; //set the red bit for the LED on
+        GPIO_PORTF_DATA_R |= LED_RED ; //or-equal sets whatever you are already superimposed with the value for red. Only valid with R/W registers
         int volatile counter = 0;
         while(counter < 1000000){
           ++counter;
         }
         
-        GPIO_PORTF_DATA_R = 0x00U ; //set the bit for the LED off
+        GPIO_PORTF_DATA_R &= ~LED_RED ; //Red off
+        //&-equals-not compares the register with the opposite of red and preforms & and since not-red will alyways disagree with the relevent bits they will be turned off leaving the rest intace.
         counter = 0;
         while(counter < 1000000){
           ++counter;
