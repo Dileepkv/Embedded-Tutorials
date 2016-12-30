8ebd8a6905d2d0c303f58c911de353341532a5b8
reversenorm <jphillips01@gmail.com>
2016-12-29 22:17:03 -0700

    I made it twinkle. ^_^
    Beginning to make sense.
    ...that only means I'll be confused later.

--- Lesson 7/main.c
+++ Lesson 7/main.c
@@ -5,21 +5,45 @@
 #define LED_GREEN (1u << 3) //0x08u
 
 int main(){
-    SYSCTL_RCGCGPIO_R |= (1u << 5) ; //turn on bit 5 of 0x400FE608 register to wake up GPIO LED memory 
-    GPIO_PORTF_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x40025400 for pin direction
-    GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x4002551c to set pins as data output 
+    SYSCTL_RCGCGPIO_R |= (1u << 5) ; //turn on bit 5 of 0x400FE608 register to wake up GPIO LED clock access 
+    SYSCTL_GPIOHBCTL_R |= (1u << 5) ; //convert processing mode to modern version AHB
+    GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x40025400 for pin direction
+    GPIO_PORTF_AHB_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x4002551c to set pins as data output 
 
-    GPIO_PORTF_DATA_BITS_R[LED_GREEN] = LED_GREEN ; //use the generic GPIO port F pointer-array and pick out the Ggreen bit: then set to green value    
-    while(1){
-      GPIO_PORTF_DATA_BITS_R[LED_RED] = LED_RED ; //use the generic GPIO port F pointer-array and pick out the first red bit: then set to red value
-        int volatile counter = 0;
-        while(counter < 1000000){
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED ;//turn red on to start
+    while(1){        
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_GREEN] = LED_GREEN ; //move to yellow    
+        int volatile counter = 0; 
+        while(counter < 500000){
           ++counter;
         }
         
-        GPIO_PORTF_DATA_BITS_R[LED_RED] =  //call up the red pointer-array red bit and simple set to 0 - or off.
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] =  //move green by turning red off
         counter = 0;
-        while(counter < 1000000){
+        while(counter < 500000){
+          ++counter;
+        }
+        
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE ; //make cyan by turning blue on
+        counter = 0;
+        while(counter < 500000){
+          ++counter;
+        }
+        
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_GREEN] = 0 ; //move to blue by turning green off    
+          while(counter < 500000){
+          ++counter;
+        }
+        
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED ; //move to purple by turning red on
+        counter = 0;
+        while(counter < 500000){
+          ++counter;
+        }
+        
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = 0 ; //turn blue off leaving us at initial red
+        counter = 0;
+        while(counter < 500000){
           ++counter;
         }
         
