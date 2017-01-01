4db1cb2a6015e58d3621c0f0c43da5ea09347989
reversenorm <jphillips01@gmail.com>
2017-01-01 13:28:23 -0700

    Recursive function and stack impact

--- Lesson 9/delay.h
+++ Lesson 9/delay.h
@@ -1,1 +1,6 @@
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
 void delay(int pause_cycles);
+
+#endif //__DELAY_H__
--- Lesson 9/main.c
+++ Lesson 9/main.c
@@ -5,8 +5,16 @@
 #define LED_BLUE (1u << 2) //0x04u
 #define LED_GREEN (1u << 3) //0x08u
 
+unsigned fact(unsigned n) ;
 
 int main(){
+    unsigned volatile x;
+    x= fact(0);
+    x= 2u + 3u*fact(1u);
+    (void)fact(5u);
+  
+  
+  
     SYSCTL_RCGCGPIO_R |= (1u << 5) ; //turn on bit 5 of 0x400FE608 register to wake up GPIO LED clock access 
     SYSCTL_GPIOHBCTL_R |= (1u << 5) ; //convert processing mode to modern version AHB
     GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x40025400 for pin direction
@@ -35,4 +43,15 @@ int main(){
         
     }
     return 0;
+}
+
+unsigned fact(unsigned n){
+  // 0! = 1
+  // n! = n*(n-1)! for all n>0
+  
+  if(n==0u){
+      return 1u;
+  }else{
+      return n*fact(n-1);
+  }
 }
