441aa4442b6f46c52d29d8acfe646f7da55f2e72
reversenorm <jphillips01@gmail.com>
2017-01-05 15:32:58 -0700

    Cleanup bad bad functions
    Learned about sneaky stack overflow caused by writing outside an array
    and corrupting return addresses.

--- Lesson 10/main.c
+++ Lesson 10/main.c
@@ -7,16 +7,8 @@
 #define WaitTime 500000u //define the time to wait between commands 
 //do this here so the variable name does not need to be stored in memory while the code runs
 
-unsigned fact(unsigned n) ;
-
 int main(){
-    unsigned volatile x;
-    x= fact(0);
-    x= 2u + 3u*fact(1u);
-    (void)fact(7u);
-  
-  
-  
+
     SYSCTL_RCGCGPIO_R |= (1u << 5) ; //turn on bit 5 of 0x400FE608 register to wake up GPIO LED clock access 
     SYSCTL_GPIOHBCTL_R |= (1u << 5) ; //convert processing mode to modern version AHB
     GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN) ; //turn on bits 1-3 of 0x40025400 for pin direction
@@ -44,18 +36,4 @@ int main(){
         
     }
     return 0;
-}
-
-unsigned fact(unsigned n){
-  // 0! = 1
-  // n! = n*(n-1)! for all n>0
-  
-  unsigned foo[6]; //this variable it going to overload the stack to learn about it
-  foo[n]=n;
-  
-  if(n==0u){
-      return 1u;
-  }else{
-      return (foo[n])*fact(n-1);
-  }
 }
